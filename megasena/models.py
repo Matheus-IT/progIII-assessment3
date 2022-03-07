6876e7f041d612f7b3e06200e8d7b471d528c99c
matheus costa <matheuscosta.tech@gmail.com>
2022-03-07 17:48:39 -0300

    created model draw

--- megasena/models.py
+++ megasena/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Draw(models.Model):
+    date = models.DateField('Data')
+    ball1 = models.IntegerField('Bola1')
+    ball2 = models.IntegerField('Bola2')
+    ball3 = models.IntegerField('Bola3')
+    ball4 = models.IntegerField('Bola4')
+    ball5 = models.IntegerField('Bola5')
+    ball6 = models.IntegerField('Bola6')
