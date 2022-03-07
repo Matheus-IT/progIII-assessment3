02d7fc7dae68f6e68d4cf1312bb010d3f792bd26
matheus costa <matheuscosta.tech@gmail.com>
2022-03-07 17:54:22 -0300

    created upload lottery file form

---
+++ megasena/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class UploadLotteryFile(forms.Form):
+    file = forms.FileField(
+        label='Arquivo tsv', allow_empty_file=False, required=True
+    )
+    start_from_scratch = forms.BooleanField(
+        label='Iniciar base do zero', required=True
+    )
