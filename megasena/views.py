b7019cb60dea9e55bbaac5075c4007e4bfb5c171
matheus costa <matheuscosta.tech@gmail.com>
2022-03-08 11:26:30 -0300

    upload lottery file template and view

--- megasena/views.py
+++ megasena/views.py
@@ -1,11 +1,48 @@
-from django.http import HttpRequest
-from django.shortcuts import render
+from datetime import datetime
+import pandas as pd
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.views import View
+
+from megasena.forms import UploadLotteryFile
+from megasena.models import Draw
 
 
 class UploadLotteryFileView(View):
     def get(self, request: HttpRequest):
-        return render(request, 'megasena/upload_lottery_file.html')
+        form = UploadLotteryFile()
+        return render(request, 'megasena/upload_lottery_file.html', locals())
 
     def post(self, request: HttpRequest):
+        form = UploadLotteryFile(request.POST, request.FILES)
+        if not form.is_valid():
+            return HttpResponse('Erro: Submeter formulário novamente!')
+
+        data = form.cleaned_data
+
+        if data['start_from_scratch']:
+            self.delete_everything()
+
+        df = pd.read_csv(data['file'], sep='\t')
+
+        for line in df.to_records():
+            date = self.convert_date(line[2])
+
+            Draw.objects.create(
+                id=line[1],
+                date=date,
+                ball1=line[3],
+                ball2=line[4],
+                ball3=line[5],
+                ball4=line[6],
+                ball5=line[7],
+                ball6=line[8],
+            )
+        return redirect('admin:index')
+
+    def delete_everything(self):
         pass
+
+    def convert_date(self, date: str) -> str:
+        """Coverts DD/MM/YYYY to YYYY-MM-DD"""
+        return datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d')
