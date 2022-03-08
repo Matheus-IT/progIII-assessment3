cda8554a0e60a67572f20aebe482fd5787f034b5
matheus costa <matheuscosta.tech@gmail.com>
2022-03-08 11:30:30 -0300

    added __str__ to model Draw

--- megasena/models.py
+++ megasena/models.py
@@ -9,3 +9,6 @@ class Draw(models.Model):
     ball4 = models.IntegerField('Bola4')
     ball5 = models.IntegerField('Bola5')
     ball6 = models.IntegerField('Bola6')
+
+    def __str__(self):
+        return f'Draw<{self.date}{self.ball1}, {self.ball2}, {self.ball3}, {self.ball4}, {self.ball5}, {self.ball6}>'
